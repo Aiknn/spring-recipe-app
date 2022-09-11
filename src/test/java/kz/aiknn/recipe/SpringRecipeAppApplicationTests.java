4bc3bac83d87cad9178d445ac49b5fd11b7b01b8
Yerlan Nurlibayev <ynurlibayev@outlook.com>
2022-09-11 21:34:58 +0600

    init: Initial commit

---
+++ src/test/java/kz/aiknn/recipe/SpringRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.aiknn.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
